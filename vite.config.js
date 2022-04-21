b717545b6fb5db9a02c5abe9af6b5d23d4613cda
310896024@qq.com <zhanhao>
2022-04-21 21:09:55 +0800

    d

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import requirePlugin from 'vite-plugin-require';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(),requirePlugin()]
 })
+
