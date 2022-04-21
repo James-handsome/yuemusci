dad152e985f13484bfec4d9d7e2db8bb24b5dd22
310896024@qq.com <zhanhao>
2022-04-21 21:37:45 +0800

    dev

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, compose, applyMiddleware } from "redux";
+import { legacy_createStore as createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducer from "./reducer";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
